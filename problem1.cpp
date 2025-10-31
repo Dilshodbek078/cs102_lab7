af553157da9dd1ce6b51c7517f0fa57a18f7d1b3
Dilshodbek078 <d.orazbayev@newuu.uz>
2025-10-31 14:23:09 +0500

    Create problem1.cpp

---
+++ problem1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int power(int x, int y) {
+
+    if (y == 0)
+        return 1;
+
+    return x * power(x, y - 1);
+}
+
+int main() {
+    int x, y;
+
+    cout << "Enter base and exponent: ";
+    cin >> x >> y;
+
+    cout << " Result =  " << power(x, y) << endl;
+
+    return 0;
+}
